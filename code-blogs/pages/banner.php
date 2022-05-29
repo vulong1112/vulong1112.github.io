91fa0e45a5e757a809b7c3bc2079f5b7879f4945
tuancodeptit <106487246+tuancodeptit@users.noreply.github.com>
2022-05-30 01:59:18 +0700

    Update banner.php

--- code-blogs/pages/banner.php
+++ code-blogs/pages/banner.php
@@ -1,6 +1,3 @@
 <div class="banner">
-            <img src="./img/học-viện.jpg" alt="banner">
-            <div class="banner-content">
-               Tổng hợp các tin tức và sự kiện của Học viện
-            </div>
+            <img src="https://qldt.ptit.edu.vn/MessageFile/banner1.jpg" alt="banner">
         </div>
